b4cd9a03698f69f3c4e320a5ff1584403c70251a
DrabantBY <yauheni.basalai@gmail.com>
2026-07-06 14:52:28 +0300

    chore: create modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ArticlesModule } from './articles/articles.module';
-import { CategoriesModule } from './categories/categories.module';
-import { CommentsModule } from './comments/comments.module';
-import { UsersModule } from './users/users.module';
+import {
+  ArticlesModule,
+  CategoriesModule,
+  CommentsModule,
+  UsersModule,
+} from './modules';
 
 @Module({
   imports: [UsersModule, ArticlesModule, CategoriesModule, CommentsModule],
