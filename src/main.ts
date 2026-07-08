29ec7fd37f0eb9e6d602b88a05203178d1e3539d
DrabantBY <yauheni.basalai@gmail.com>
2026-07-08 21:52:06 +0300

    feat: add swagger base config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 process.loadEnvFile();
 
@@ -13,6 +14,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
