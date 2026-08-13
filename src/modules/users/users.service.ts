0433b60d8dc45c6a254cc93a51e2c1f3431c5940
DrabantBY <yauheni.basalai@gmail.com>
2026-08-13 22:59:15 +0300

    devs: add bcryptjs

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,22 +3,19 @@ import { ArticlesService } from '@articles/articles.service';
 import { BaseEntityService } from '@common/services';
 import type { PaginationResponse } from '@common/types';
 import { idNotFoundMessage } from '@common/utils';
-import {
-  ForbiddenException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import bcrypt from 'bcryptjs';
 import { USER_ROLE } from './const';
-import type {
-  CreateUserDto,
-  UpdatePasswordDto,
-  UserSearchParamsDto,
-} from './dto';
+import type { CreateUserDto, UpdatePasswordDto, UserSearchParamsDto } from './dto';
 import { User } from './entities';
 
 @Injectable()
 export class UsersService extends BaseEntityService<User> {
-  constructor(private articleService: ArticlesService) {
+  constructor(
+    private articleService: ArticlesService,
+    private configService: ConfigService,
+  ) {
     super();
   }
 
@@ -41,16 +38,16 @@ export class UsersService extends BaseEntityService<User> {
     return user;
   }
 
-  async insertOne({
-    login,
-    password,
-    role = USER_ROLE.VIEWER,
-  }: CreateUserDto): Promise<User> {
+  async insertOne({ login, password, role = USER_ROLE.VIEWER }: CreateUserDto): Promise<User> {
     const date = Date.now();
+
+    const CRYPT_SALT = Number(this.configService.get<string>('CRYPT_SALT')) || 10;
+    const bcryptPassword = await bcrypt.hash(password, CRYPT_SALT);
+
     const user: User = new User({
       id: randomUUID(),
       login,
-      password,
+      password: bcryptPassword,
       role,
       createdAt: date,
       updatedAt: date,
@@ -59,14 +56,17 @@ export class UsersService extends BaseEntityService<User> {
     return user;
   }
 
-  async updateOne(
-    id: string,
-    { oldPassword, newPassword }: UpdatePasswordDto,
-  ): Promise<User> {
+  async updateOne(id: string, { oldPassword, newPassword }: UpdatePasswordDto): Promise<User> {
     const user = await this.fetchOne(id);
-    if (user.password !== oldPassword)
-      throw new ForbiddenException(`Old password is wrong`);
-    user.password = newPassword;
+
+    const isPasswordsEqual = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isPasswordsEqual) throw new ForbiddenException(`Old password is wrong`);
+
+    const CRYPT_SALT = Number(this.configService.get<string>('CRYPT_SALT')) || 10;
+    const bcryptPassword = await bcrypt.hash(newPassword, CRYPT_SALT);
+
+    user.password = bcryptPassword;
     user.updatedAt = Date.now();
     return user;
   }
