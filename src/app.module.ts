92bee7a666c922c688b89d0dc2022c15e589bc8a
DrabantBY <yauheni.basalai@gmail.com>
2026-08-14 23:27:00 +0300

    feat: import global config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
 import { LoggerMiddleware } from '@common/middlewares';
-import {
-  type MiddlewareConsumer,
-  Module,
-  type NestModule,
-} from '@nestjs/common';
+import { type MiddlewareConsumer, Module, type NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-import {
-  ArticlesModule,
-  CategoriesModule,
-  CommentsModule,
-  UsersModule,
-} from './modules';
+import { ArticlesModule, CategoriesModule, CommentsModule, UsersModule } from './modules';
 
 @Module({
-  imports: [UsersModule, ArticlesModule, CategoriesModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule,
+    ArticlesModule,
+    CategoriesModule,
+    CommentsModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
